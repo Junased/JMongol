3c9f5e4cdbc81eef609287a2268ea1330ac95039
Junased <junased@gmail.com>
2015-05-05 22:37:28 +0800

    本次提交主要是完善了DBHelp层的query部分，即负责从数据库查询数据的部分，还是ado.net部分，继承了DBHelp的base部分

--- JMongolDBHelp/QueryDBHelp.cs
+++ JMongolDBHelp/QueryDBHelp.cs
@@ -86,7 +86,7 @@ namespace JMongolDBHelp
         }
 
         /// <summary>
-        /// 执行存储过程，返回数据集dataset
+        /// 执行存储过程(无返回值)，返回数据集dataset
         /// </summary>
         /// <typeparam name="T">待装载的数据类型</typeparam>
         /// <param name="storeProcName">存储过程名</param>
@@ -103,7 +103,7 @@ namespace JMongolDBHelp
                     try
                     {
                         conn.Open();
-                        da.SelectCommand = CreateSQLCommandForStoreProcedureWithReturn(conn, storeProcName, parameters);
+                        da.SelectCommand = CreateSQLCommendForStoreProcedureWithoutReturn(conn, storeProcName, parameters);
                         da.Fill(t, tableName);
                         return t;
                     }
@@ -121,7 +121,134 @@ namespace JMongolDBHelp
 
         #endregion
 
-         
+        #region 查询满足条件的记录数
 
+
+        /// <summary>
+        /// 执行查询语句，返回满足查询条件的记录数
+        /// </summary>
+        /// <typeparam name="T">待装载的数据类型</typeparam>
+        /// <param name="strCount">查询记录数</param>
+        /// <param name="strSQL">查询语句</param>
+        /// <param name="start">查询开始的位置</param>
+        /// <param name="count">要查询的记录数</param>
+        /// <param name="isPage">是否分页</param>
+        /// <param name="t">待装载的数据实例</param>
+        /// <returns>满足查询条件的记录数</returns>
+        public static int QueryDataTable<T>(string strCount, string strSQL, int start, int count, bool isPage, T t) where T : DataTable
+        {
+            using (SqlConnection conn = new SqlConnection(BaseDBHelp.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand comm = new SqlCommand(strCount, conn);
+                    var obj = comm.ExecuteScalar();
+
+                    SqlDataAdapter da = new SqlDataAdapter(strSQL, conn);
+                    if (count == 0 || isPage == true)
+                    {
+                        da.Fill(t);
+                    }
+                    else
+                    {
+                    }
+                    return int.Parse(obj.ToString());
+                }
+                catch (SqlException ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 执行查询语句，返回满足查询条件的记录数
+        /// </summary>
+        /// <typeparam name="T">待装载的数据类型</typeparam>
+        /// <param name="strCount">查询记录数</param>
+        /// <param name="strSQL">查询语句</param>
+        /// <param name="start">查询开始的位置</param>
+        /// <param name="count">要查询的记录数</param>
+        /// <param name="t">待装载的数据实例</param>
+        /// <returns>满足查询条件的记录数</returns>
+        public static int QueryDataTable<T>(string strCount, string strSQL, int start, int count, T t) where T : DataTable
+        {
+            return QueryDBHelp.QueryDataTable(strCount, strSQL, start, count, false, t);
+        }
+
+
+        /// <summary>
+        /// 执行查询语句，并且带有参数，返回满足查询条件的记录数
+        /// </summary>
+        /// <typeparam name="T">待装载的数据类型</typeparam>
+        /// <param name="strSQL">查询语句</param>
+        /// <param name="t">待装载的数据实例</param>
+        /// <param name="parameters">参数</param>
+        /// <returns>返回满足实例的记录数</returns>
+        public static int QueryDataTable<T>(string strSQL, T t, params SqlParameter[] parameters) where T : DataTable
+        {
+            using (SqlConnection conn=new SqlConnection(BaseDBHelp.ConnectionString))
+            {
+                SqlCommand comm = new SqlCommand();
+                BaseDBHelp.PrepareCommand(comm, conn, null, strSQL, parameters);
+                using (SqlDataAdapter da = new SqlDataAdapter(comm))
+                {
+                    try
+                    {
+                        da.Fill(t);
+                        comm.Parameters.Clear();
+                        return t.Rows.Count;
+                    }
+                    catch (SqlException ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行存储过程，并带有参数，返回满足查询条件的数据记录数
+        /// </summary>
+        /// <typeparam name="T">待装载的数据类型</typeparam>
+        /// <param name="storeProcedureName">存储过程名</param>
+        /// <param name="t">待装载的数据实例</param>
+        /// <param name="parameters">参数</param>
+        /// <returns>返回满足查询条件的数据记录数</returns>
+        public static int QueryDataTable<T>(string storeProcedureName, SqlParameter[] parameters,T t) where T : DataTable
+        {
+            using (SqlConnection conn=new SqlConnection(BaseDBHelp.ConnectionString))
+            {
+                using (SqlDataAdapter da=new SqlDataAdapter())
+                {
+                    try
+                    {
+                        conn.Open();
+                        da.SelectCommand = CreateSQLCommendForStoreProcedureWithoutReturn(conn, storeProcedureName, parameters);
+                        da.Fill(t);
+                        return t.Rows.Count;
+                    }
+                    catch (SqlException ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+        } 
+	#endregion
     }
 }
