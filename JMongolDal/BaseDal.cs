d10a93feb7e8a849416df0f7dcffaf2eb3c2d009
Junased <junased@gmail.com>
2015-05-06 23:25:10 +0800

    创建dal层，该层主要负责业务的具体实现，比如拼接SQL语句等，本次提交完成了base部分的属性部分

---
+++ JMongolDal/BaseDal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+
+using JMongolDBHelp;
+using JMongolModel;
+
+namespace JMongolDal
+{
+    public class BaseDal
+    {
+        #region 常用属性
+
+        /// <summary>
+        /// 数据表中删除标记为false的条件
+        /// </summary>
+        protected string DeleteFalse
+        {
+            get
+            {
+                return Const.Delete + Const.EqualMark + Const.Zero;
+            }
+        }
+
+        /// <summary>
+        /// 数据表中删除标记为true的条件
+        /// </summary>
+        protected string DeleteTrue
+        {
+            get
+            {
+                return Const.Delete + Const.EqualMark + Const.One;
+            }
+        } 
+        #endregion
+    }
+}
