f9408a398617f803f1d49bd1ec70dcba912e2951
Junased <junased@gmail.com>
2015-09-18 23:42:04 +0800

    初步完成权限的Dal部分

---
+++ JMongolDal/Admin/PermissionDal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+using JMongolModel;
+using JMongolModel.Entity;
+using JMongolModel.Table;
+using JMongolDBHelp;
+
+namespace JMongolDal.Admin
+{
+    public class PermissionDal : BaseDal
+    {
+        public int GetPermissionList(string roleID, AdminDataSet.tPermissionDataTable table)
+        {
+            string strSql = Const.Select + " p." + Const.StarSign + tPermission.Name + " p " + Const.InnerJoin + tRolePermission.Name + " t " + Const.ON + " p." + Const.ID + Const.EqualMark + " t." + tRolePermission.PermissionID + Const.Where + " t." + tRolePermission.RoleID + Const.EqualMark + Const.AT + tRolePermission.RoleID;
+            SqlParameter[] para = { new SqlParameter(Const.AT + tRolePermission.RoleID, roleID) };
+            return BaseDBHelp.ExecuteSql(strSql, para);
+        }
+    }
+}
