8d80cd72df26b1f45d466bf0d7247c43f2dc9e53
Junased <junased@gmail.com>
2015-09-18 23:46:08 +0800

    添加一些注释

--- JMongolDal/Admin/PermissionDal.cs
+++ JMongolDal/Admin/PermissionDal.cs
@@ -15,6 +15,12 @@ namespace JMongolDal.Admin
 {
     public class PermissionDal : BaseDal
     {
+        /// <summary>
+        /// 获取权限列表
+        /// </summary>
+        /// <param name="roleID">角色ID</param>
+        /// <param name="table">待填充的数据集</param>
+        /// <returns>返回查询的数据行</returns>
         public int GetPermissionList(string roleID, AdminDataSet.tPermissionDataTable table)
         {
             string strSql = Const.Select + " p." + Const.StarSign + tPermission.Name + " p " + Const.InnerJoin + tRolePermission.Name + " t " + Const.ON + " p." + Const.ID + Const.EqualMark + " t." + tRolePermission.PermissionID + Const.Where + " t." + tRolePermission.RoleID + Const.EqualMark + Const.AT + tRolePermission.RoleID;
